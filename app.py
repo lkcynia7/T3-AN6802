ac6cb416c310800d001d5893710b0c65bd582a3a
lkcynia7 <C240047@e.ntu.edu.sg>
2025-04-08 02:21:05 +0000

    Apr 8

--- app.py
+++ app.py
@@ -1,13 +1,18 @@
 from flask import Flask, request, render_template
+import requests
 import sqlite3
 import datetime
 import google.generativeai as genai
 import os
 import wikipedia
+import time
 
 api = os.getenv("makersuite")
 model = genai.GenerativeModel("gemini-1.5-flash")
 genai.configure(api_key=api)
+
+TELEGRAM_API = os.getenv("lkcynia_bot")
+BASE_URL = f"https://api.telegram.org/bot{TELEGRAM_API}/"
 
 app = Flask(__name__)
 flag = 1
@@ -74,6 +79,36 @@ def FAQinput():
     q = request.form.get("q")
     r = wikipedia.summary(q)
     return render_template("FAQinput.html", r=r)
+
+@app.route('/telegram', methods=['GET','POST'])
+def telegram():
+    #grab id
+    time.sleep(5) #ensure telegram is activated
+    response = requests.get(BASE_URL + 'getUpdates')
+    data = response.json()
+    text = data['result'][-1]['message']['text']
+    chat_id = data['result'][-1]['message']['chat']['id']
+    print("Text:", text)
+    print("Chat ID:", chat_id)
+    send_url = BASE_URL + f'sendMessage?chat_id={chat_id}&text={"Welcome to prediction, please enter the salary"}'
+    requests.get(send_url)
+    time.sleep(6)
+    response = requests.get(BASE_URL + 'getUpdates')
+    data = response.json()
+    text = data['result'][-1]['message']['text']
+    if text.isnumeric():
+        msg = str(float(text) * 0.2 + 100)
+        send_url = BASE_URL + f'sendMessage?chat_id={chat_id}&text={msg}'
+        requests.get(send_url)
+    else:
+        msg = "salary must be a number"
+        send_url = BASE_URL + f'sendMessage?chat_id={chat_id}&text={msg}'
+        requests.get(send_url)
+        send_url = BASE_URL + f'sendMessage?chat_id={chat_id}&text={"Welcome to prediction, please enter the salary"}'
+        requests.get(send_url)
+        time.sleep(3)
+    return(render_template("telegram.html"))
+
 
 @app.route("/userLog", methods=["POST","GET"])
 def userLog():
